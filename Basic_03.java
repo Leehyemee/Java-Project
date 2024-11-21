b5288e9746925c9fa4d22f6b70da355690bc2e7f
Lee Hyemee <svsvdvdv@naver.com>
2024-11-21 17:32:03 +0900

    no message

---
+++ Basic_03.java
@@ -0,0 +1,21 @@
+package basic;
+
+/*
+ * 자바에서의 제어 문자
+ * 
+ * - \t : 일정한 간격(tab)만큼 띄어쓰기를 해 주는 제어문자.
+ * - \n : 줄바꿈을 제공해 주는 제어 문자.
+ */
+public class Basic_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("프로그램 시작");
+		
+		System.out.print("\njava");
+		
+		System.out.println("\tprogramming");
+
+	}
+
+}
